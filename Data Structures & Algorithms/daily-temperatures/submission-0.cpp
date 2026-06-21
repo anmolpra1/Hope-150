bf2a5ead912ddd58f9417edea615b9393da32c8a
Anmol Prajapati <167869008+anmolpra1@users.noreply.github.com>
2026-06-21 14:25:45 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,20 @@
+// MONOTONIC DEC STACK  
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> st;  // STORES INDEX
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && temperatures[i] >= temperatures[st.top()]) {
+                st.pop();
+            }
+            ans[i] = st.empty() ? 0 : st.top() - i;
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
