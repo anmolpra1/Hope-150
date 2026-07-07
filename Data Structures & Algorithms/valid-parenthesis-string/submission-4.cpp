f37fe43af80ab484dc4fc97ade3b31352ebab409
Anmol Prajapati <167869008+anmolpra1@users.noreply.github.com>
2026-07-08 00:14:18 +0530

    Add: valid-parenthesis-string - submission-4

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-4.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> left;
+        stack<int> star;
+
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                left.push(i);
+            }
+
+            else if (s[i] == '*') {
+                star.push(i);
+            }
+
+            else {
+                if (!left.empty()) {
+                    left.pop();
+                }
+
+                else if (!star.empty()) {
+                    star.pop();
+                }
+
+                else {
+                    return false;
+                }
+            }
+        }
+
+        while (!left.empty() && !star.empty()) {
+            if (left.top() > star.top()) return false;
+            left.pop();
+            star.pop();
+        }
+        return left.empty();
+    }
+};
